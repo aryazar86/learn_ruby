187b5a20ccbb1ef23ef2ca39129779aec0969cc8
Arya Iranpour <arya.iranpour@gmail.com>
2013-10-29 13:01:57 -0400

    Finished book_titles.rb

--- 07_book_titles/book.rb
+++ 07_book_titles/book.rb
@@ -1,10 +1,21 @@
 class Book
 
-  # def title(string)
-  #   # your code here
-  # end
   def title=(string)
-    string.capitalize!
+    @book_title = string
+  end
+
+  def title
+
+    arr = @book_title.split
+    @book_title = ""
+
+    arr.each do |x| 
+      x.capitalize! if arr.index(x) == 0 || x != "the" && x != "a" && x != "an" && x != "and" && x != "of" && x != "in" if x.to_i.to_s != x
+      @book_title << x + " "
+    end
+    
+    @book_title.chop
+  
   end
 
 end
